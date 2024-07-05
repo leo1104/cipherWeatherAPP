e42e1c9a07c860865e9be9e61c780ada562468d9
Prantosh Verma <prantoshv@gmail.com>
2024-07-05 10:50:31 +0530

    homework

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+
 
 function Weather() {
+
+//extract lat and lon from the URL
+
+
+//call axios inside a function with 
+//http://api.weatherapi.com/v1/current.json?key=<YourAPIKEY>&q=lat,lon&aqi=no
+//http://api.weatherapi.com/v1/forecast.json?key=<YourAPIKEY>&q=lat,lon&days=1&aqi=no&alerts=no
+
+// the above 2 API's
+
+//extract the response data in 2 states and represent the data in the render part to the user
+  
+
   return (
     <div>Weather</div>
   )
